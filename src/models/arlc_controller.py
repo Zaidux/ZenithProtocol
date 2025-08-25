db8f602be8ad8e9d4a9264e692acba21916c6a2f
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-08-25 17:13:49 +0100

    Update arlc_controller.py

--- src/models/arlc_controller.py
+++ src/models/arlc_controller.py
@@ -1,1 +1,87 @@
+# /src/model/arlc_controller.py
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from .asreh_model import ASREHModel
+from ..data.tetris_generator import MemoryEfficientDataset
+from ..data.chess_generator import ChessDataset
+import random
+
+class ARLCController:
+    def __init__(self, model, lr=1e-4):
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=lr)
+        self.loss_fn = nn.MSELoss()
+        
+    def _evaluate_action(self, conceptual_before, conceptual_after):
+        """
+        Calculates a reward based on the change in conceptual features.
+        This is a dynamic, abstract reward function.
+        """
+        # Example for chess: reward for improved king safety and material advantage
+        # conceptual_features: [material_adv, white_king_safety, black_king_safety, ...]
+        material_change = conceptual_after[:, 0] - conceptual_before[:, 0]
+        white_safety_change = conceptual_after[:, 1] - conceptual_before[:, 1]
+        
+        reward = material_change - white_safety_change # Simple heuristic
+        return reward
+
+    def train_step(self, data_loader, domain: str):
+        self.model.train()
+        total_loss = 0
+        
+        for batch in data_loader:
+            if domain == 'tetris':
+                state_before, board_after, next_piece_idx, conceptual_features_before = batch
+                
+                # Use the model to predict the next state
+                predicted_next_state, _ = self.model(state_before, conceptual_features_before, 'tetris')
+                
+                # This part would require a custom Tetris environment to get conceptual features after the move.
+                # For demonstration, we'll use a simple conceptual loss.
+                # Here, we'd need to simulate the action and get `conceptual_after`.
+                # Loss is a combination of supervised and reinforced loss.
+                loss = self.loss_fn(predicted_next_state, board_after.float())
+                
+            elif domain == 'chess':
+                state_before, move_idx, conceptual_features_before = batch
+                
+                # Use the model to predict the next move
+                predicted_move, _ = self.model(state_before, conceptual_features_before, 'chess')
+                
+                # ARLC logic: We need to get the "conceptual_after" state.
+                # This would typically be handled by the environment in a real training loop.
+                # For this example, we'll use the provided `conceptual_features_after`.
+                
+                # ARLC Reward Signal (simplified)
+                # reward = self._evaluate_action(conceptual_features_before, conceptual_features_after)
+                # This would be used to compute a reinforcement learning loss component.
+                
+                # For now, let's use a simple supervised loss on the move itself
+                loss = F.cross_entropy(predicted_move, move_idx)
+            
+            else:
+                raise ValueError("Invalid domain.")
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+            
+        return total_loss / len(data_loader)
+
+# --- Example Usage ---
+# asreh_model = ASREHModel()
+# arlc_controller = ARLCController(asreh_model)
+# tetris_dataset = MemoryEfficientDataset()
+# chess_dataset = ChessDataset()
+#
+# tetris_loader = DataLoader(tetris_dataset, batch_size=32)
+# chess_loader = DataLoader(chess_dataset, batch_size=32)
+#
+# # Train on both domains
+# # arlc_controller.train_step(tetris_loader, 'tetris')
+# # arlc_controller.train_step(chess_loader, 'chess')
 
