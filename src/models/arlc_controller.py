dedd1b5bc984ece018041d331aca26aa84c850c9
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-08-25 20:03:37 +0100

    Update arlc_controller.py

--- src/models/arlc_controller.py
+++ src/models/arlc_controller.py
@@ -8,24 +8,23 @@ from typing import Dict, List, Tuple
 from ..utils.config import Config
 from ..games.tetris_env import get_conceptual_features as get_tetris_features
 from ..games.chess_env import get_conceptual_features as get_chess_features
-
-# We don't need the other imports from tetris_env.py in the ARLC,
-# as it should be agnostic to the environment's internal mechanics.
+from .hyper_conceptual_thinking import ConceptDiscoveryEngine
 
 class ARLCController:
     """
     The Adaptive Reinforcement Learning Controller (ARLC) is the "Brain" of the system.
     It evaluates possible moves and chooses the best one based on a scoring heuristic.
-    This version is designed to be domain-agnostic for the Split Mind (SPM) architecture.
+    This version is designed to be domain-agnostic and orchestrates the HCT process.
     """
     def __init__(self, exploration_weight: float = 5.0):
         self.exploration_weight = exploration_weight
         self.visited_states = {}
         self.reward_coeffs = Config.ARLC_REWARD_COEFFS
+        self.cde = ConceptDiscoveryEngine()
 
-    def evaluate_conceptual_features(self, conceptual_features: np.ndarray, domain: str) -> Dict[str, float]:
+    def evaluate_conceptual_features(self, conceptual_features: np.ndarray, fused_representation: torch.Tensor, domain: str) -> Dict[str, float]:
         """
-        Calculates a score for a given set of conceptual features.
+        Calculates a score for a given set of conceptual features and adds an HCT bonus.
         This function is the core of the ARLC's reward mechanism.
         """
         score = 0
@@ -46,11 +45,16 @@ class ARLCController:
         exploration_bonus = self.exploration_weight / (1 + visits)
         self.visited_states[state_hash] = visits + 1
         
-        final_score = score + exploration_bonus
+        # HCT: Check for newly discovered high-value concepts and add a bonus
+        hct_bonus, discovered_concept = self.cde.analyze_for_new_concepts(fused_representation, score, domain)
+        
+        final_score = score + exploration_bonus + hct_bonus
 
         return {
             "conceptual_score": score,
             "exploration_bonus": exploration_bonus,
+            "hct_bonus": hct_bonus,
+            "discovered_concept": discovered_concept,
             "score": final_score
         }
 
