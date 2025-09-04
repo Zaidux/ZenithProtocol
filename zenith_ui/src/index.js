2a8b1dd1132d9682e5601ae3f6407e58d56192ae
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-04 09:27:18 +0100

    Create index.js

---
+++ zenith_ui/src/index.js
@@ -0,0 +1,15 @@
+// zenith_ui/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/global.css';
+import './styles/themes.css';
+import './styles/components.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
